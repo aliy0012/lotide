0606b4c74a8e3d143f4405fb1c4b18652c3c2521
aliy0012 <alikamal415@yahoo.com>
2021-11-01 19:47:32 +0000

    refactored middle.js function

--- middle.js
+++ middle.js
@@ -1,26 +1,7 @@
-const eqArrays = function(arr1, arr2) {
-  for ( firstArrayElement of arr1) {
-    for( secondArrayElement of arr2){
-      if(firstArrayElement === secondArrayElement){
-        continue;
-      }
-      else if (firstArrayElement === secondArrayElement){
-        return false;
-      }
-    }
-  }
-  return true;
 
-};
+const eqArrays = require('./eqArrays.js');
+const assertArraysEqual = require('./assertArraysEqual');
 
-const assertArraysEqual = function (arr1 , arr2){
-  if(eqArrays(arr1, arr2)){
-    console.log('✅✅✅ Assertion Passed: ${arr1} === ${arr2}');
-    return true;
-  }
-  console.log('🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}');
-  return false;
-};
 
 const middle = function (someArray) {
   let arr = [];
@@ -42,7 +23,4 @@ const middle = function (someArray) {
 
 };
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
