a137bb5b610d5d6fa2303913e089496e148c9350
aliy0012 <alikamal415@yahoo.com>
2021-11-01 23:15:53 +0000

    mocha and chai testing added

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,21 @@
-const head = require('../head.js');
-const log = console.log;
+// const head = require('../head.js');
+// const log = console.log;
 
-log(head(234234));
-log(head("Lighthouse"));
-log(head('bootcamp'));
+// log(head(234234));
+// log(head("Lighthouse"));
+// log(head('bootcamp'));
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+
+describe("#head", () => {
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
